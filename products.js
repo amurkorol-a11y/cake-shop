50739ab5c0fe0b1d31d1881fdf44caf5aaee84b6
amurkorol-a11y <amurkorol@gmail.com>
2026-03-11 13:10:07 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,74 +1,92 @@
 window.BAKARY_PRODUCTS = [
-  { 
-    id: 1, type: 'set4',
-    name: 'Павлова с черникой и лимонным курдом (4 шт)',
-    price: 325, weight: '4 шт',
+  {
+    id: 1,
+    type: 'set4',
+    name: 'Пирожное "Клубничный шик" (4 шт)',
+    price: 375,
+    weight: '4 шт',
+    description: 'Мусс, клубничное компоте, миндаль',
+    flowwowLink: 'https://flowwow.com/bakery-products/pirozhnoe-klubnichniy-shik/',
+    image: 'https://sun9-58.userapi.com/s/v1/ig2/4tZCTMC9SX13U_Wuf4W-MNFfX-qxdhf3rGSMjRPYN98rPcbyP2DI-Ti4_FoAKhyYygq3n8xWpQdn4NX6GqUqXhjl.jpg?quality=95&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,960x1280&from=bu&cs=480x0'
+  },
+  {
+    id: 2,
+    type: 'set4',
+    name: 'Павлова бенто с крем-чизом и ягодами',
+    price: 325,
+    weight: '4 шт',
     description: 'Воздушная меренга, сочная черника и лимонный курд',
-    flowwowLink: 'https://flowwow.com/product/pavlova-chernika',
+    flowwowLink: 'https://flowwow.com/bakery-products/pavlova-bento/',
     image: 'https://sun9-46.userapi.com/s/v1/ig2/cK7yuBzfPq-caUeAANAHeb0Y6youpfTBYezQapz1kR-tVgcSaM3AGu_PmALhVwFp6_2ytROOAso-dlG-SQsJ9_l1.jpg?quality=95&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,960x1280&from=bu&cs=480x0'
   },
-  { 
-    id: 2, type: 'weight',
-    name: 'Мадлен с персиком (350 г)',
-    price: 1000, weight: '350 г',
-    description: 'Нежные бисквитные пирожные в белом шоколаде',
-    flowwowLink: 'https://flowwow.com/product/madlen-persik',
-    image: 'https://sun9-3.userapi.com/s/v1/ig2/UoLdQhQnfmx1CZe7vzes5KrkrTYYsMBnCcWEpvBXbJfPaTj_SE-MwU2XFdc2hxvzjxH_U7_7HDOlQzNfIhlQ41IJ.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280&from=bu&cs=480x0'
-  },
-  { 
-    id: 3, type: 'set4',
-    name: 'Муссовые киви/клубника (4 шт)',
-    price: 350, weight: '4 шт',
-    description: 'Мусс с начинкой из свежих ягод',
-    flowwowLink: 'https://flowwow.com/product/muss-kiwi-klubnika',
-    image: 'https://sun9-42.userapi.com/s/v1/ig2/bvu5OkGnAV08ejApuyNQNAaR6WogyXHAyZbes4-8XKPwIhnwTbiD0n70bR1Twe63BQDtIiuApRwVJOl88Tz1HcNh.jpg?quality=95&as=32x49,48x74,72x111,108x167,160x247,240x371,360x556,480x741,540x834,640x988,720x1112,829x1280&from=bu&cs=480x0'
-  },
-  { 
-    id: 4, type: 'set4',
-    name: 'Муссовые апельсин (4 шт)',
-    price: 350, weight: '4 шт',
-    description: 'Яркий мусс с апельсиновой начинкой',
-    flowwowLink: 'https://flowwow.com/product/muss-apelsin',
-    image: 'https://sun9-42.userapi.com/s/v1/ig2/bvu5OkGnAV08ejApuyNQNAaR6WogyXHAyZbes4-8XKPwIhnwTbiD0n70bR1Twe63BQDtIiuApRwVJOl88Tz1HcNh.jpg?quality=95&as=32x49,48x74,72x111,108x167,160x247,240x371,360x556,480x741,540x834,640x988,720x1112,829x1280&from=bu&cs=480x0'
-  },
-  { 
-    id: 5, type: 'weight',
-    name: 'Зефир натуральный «Клюква» (350 г)',
-    price: 850, weight: '350 г',
-    description: 'Зефир с насыщенным вкусом свежей клюквы',
-    flowwowLink: 'https://flowwow.com/product/zefir-klukva',
-    image: 'https://sun9-46.userapi.com/s/v1/ig2/3UHJrY_j41ibzGK48S77AvKNTzyV8zbJaYZhkO7uqZAGZClKtmJv32BINjTP5H0FokU0fmFqLQoZg9ujdtip3mWd.jpg?quality=95&as=32x71,48x107,72x161,108x241,160x357,240x536,360x804,480x1072,540x1206,573x1280&from=bu&cs=480x0'
-  },
-  { 
-    id: 6, type: 'set4',
-    name: 'Павлова «Ягодный крем-чиз» (4 шт)',
-    price: 325, weight: '4 шт',
-    description: 'Меренга, ягоды, воздушный крем-чиз',
-    flowwowLink: 'https://flowwow.com/product/pavlova-yagoda',
+  {
+    id: 3,
+    type: 'set4',
+    name: 'Набор меренговых пирожных с клубникой (4 шт)',
+    price: 325,
+    weight: '4 шт',
+    description: 'Воздушная меренга, сочная черника и лимонный курд',
+    flowwowLink: 'https://flowwow.com/bakery-products/nabor-pirozhnyh-klubnika/',
     image: 'https://sun9-36.userapi.com/s/v1/ig2/KR06rCpX9GrYOREYfAYozf45hYd9q7IQOiKrNeiW7476S5ZQyPEnwoHREzpq7IKWWOAUoyPC-7etiQQC-tetorfk.jpg?quality=95&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,960x1280&from=bu&cs=480x0'
   },
-  { 
-    id: 7, type: 'weight',
-    name: 'Меренговые «Клубника» (250 г)',
-    price: 1300, weight: '250 г',
-    description: 'Хрустящая меренга с клубничным компоте',
-    flowwowLink: 'https://flowwow.com/product/merenga-klubnika',
-    image: 'https://sun9-15.userapi.com/s/v1/ig2/t4R6xo0xMufxBpxi_z-e1uXZlvhj5VRm4s4PBjjiRiGo0j_DHjhy0j-u-fLDJ2KWO6eOSb_idnkZdyb5EHYu8cN_.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280&from=bu&cs=480x0'
+  {
+    id: 4,
+    type: 'set4',
+    name: 'Набор меренга и клубника (4 шт)',
+    price: 325,
+    weight: '4 шт',
+    description: 'Мусс с начинкой из свежих ягод',
+    flowwowLink: 'https://flowwow.com/bakery-products/nabor-merenga-i-klubnika/',
+    image: 'https://sun9-42.userapi.com/s/v1/ig2/bvu5OkGnAV08ejApuyNQNAaR6WogyXHAyZbes4-8XKPwIhnwTbiD0n70bR1Twe63BQDtIiuApRwVJOl88Tz1HcNh.jpg?quality=95&as=32x49,48x74,72x111,108x167,160x247,240x371,360x556,480x741,540x834,640x988,720x1112,829x1280&from=bu&cs=480x0'
   },
-  { 
-    id: 8, type: 'weight',
-    name: 'Зефир натуральный «Банан» (350 г)',
-    price: 850, weight: '350 г',
+  {
+    id: 5,
+    type: 'weight',
+    name: 'Набор Мадлен Клюква (350 г)',
+    price: 1000,
+    weight: '350 г',
+    description: 'Нежные бисквитные пирожные в белом шоколаде',
+    flowwowLink: 'https://flowwow.com/bakery-products/nabor-madlen-2158/',
+    image: 'https://sun9-46.userapi.com/s/v1/ig2/3UHJrY_j41ibzGK48S77AvKNTzyV8zbJaYZhkO7uqZAGZClKtmJv32BINjTP5H0FokU0fmFqLQoZg9ujdtip3mWd.jpg?quality=95&as=32x71,48x107,72x161,108x241,160x357,240x536,360x804,480x1072,540x1206,573x1280&from=bu&cs=480x0'
+  },
+  {
+    id: 6,
+    type: 'weight',
+    name: 'Набор мадлен Персик (350 г)',
+    price: 1000,
+    weight: '350 г',
+    description: 'Нежные бисквитные пирожные в белом шоколаде',
+    flowwowLink: 'https://flowwow.com/bakery-products/nabor-madlen-persik/',
+    image: 'https://sun9-3.userapi.com/s/v1/ig2/UoLdQhQnfmx1CZe7vzes5KrkrTYYsMBnCcWEpvBXbJfPaTj_SE-MwU2XFdc2hxvzjxH_U7_7HDOlQzNfIhlQ41IJ.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280&from=bu&cs=480x0'
+  },
+  {
+    id: 7,
+    type: 'weight',
+    name: 'Зефир банан (350 г)',
+    price: 850,
+    weight: '350 г',
     description: 'Зефир со вкусом спелого банана',
-    flowwowLink: 'https://flowwow.com/product/zefir-banan',
+    flowwowLink: 'https://flowwow.com/bakery-products/zefir-banan/',
     image: 'https://sun9-66.userapi.com/s/v1/ig2/ThlU_SFO4YFhIdxeR65qMaUNSyugHm7jWXnD6JM5rIn6LAWhc2pbVq-NzkgrOcyp2mODnfUfJ4ss7x5VWMqCUVMs.jpg?quality=95&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,960x1280&from=bu&cs=480x0'
   },
-  { 
-    id: 9, type: 'set4',
-    name: 'Муссовые «Клубничный шик» (4 шт)',
-    price: 375, weight: '4 шт',
-    description: 'Мусс, клубничное компоте, миндаль',
-    flowwowLink: 'https://flowwow.com/product/muss-klubnika-shik',
-    image: 'https://sun9-58.userapi.com/s/v1/ig2/4tZCTMC9SX13U_Wuf4W-MNFfX-qxdhf3rGSMjRPYN98rPcbyP2DI-Ti4_FoAKhyYygq3n8xWpQdn4NX6GqUqXhjl.jpg?quality=95&as=32x43,48x64,72x96,108x144,160x213,240x320,360x480,480x640,540x720,640x853,720x960,960x1280&from=bu&cs=480x0'
+  {
+    id: 8,
+    type: 'weight',
+    name: 'Зефир клюква (350 г)',
+    price: 850,
+    weight: '350 г',
+    description: 'Зефир с насыщенным вкусом свежей клюквы',
+    flowwowLink: 'https://flowwow.com/bakery-products/zefir-klyukva-7194/',
+    image: 'https://sun9-46.userapi.com/s/v1/ig2/3UHJrY_j41ibzGK48S77AvKNTzyV8zbJaYZhkO7uqZAGZClKtmJv32BINjTP5H0FokU0fmFqLQoZg9ujdtip3mWd.jpg?quality=95&as=32x71,48x107,72x161,108x241,160x357,240x536,360x804,480x1072,540x1206,573x1280&from=bu&cs=480x0'
+  },
+  {
+    id: 9,
+    type: 'set4',
+    name: 'Набор пирожных Павлова/Меренги (4 шт)',
+    price: 325,
+    weight: '4 шт',
+    description: 'Меренга, ягоды, воздушный крем-чиз',
+    flowwowLink: 'https://flowwow.com/flowers/nabor-pirozhnyh-pavlova-1127/',
+    image: 'https://sun9-15.userapi.com/s/v1/ig2/t4R6xo0xMufxBpxi_z-e1uXZlvhj5VRm4s4PBjjiRiGo0j_DHjhy0j-u-fLDJ2KWO6eOSb_idnkZdyb5EHYu8cN_.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280&from=bu&cs=480x0'
   }
 ];
